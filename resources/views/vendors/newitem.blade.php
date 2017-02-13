4024cde14007e9a0391f7d84d4e25f63a2f438ca
HlaingTinHtun <hlaingtinhtun@gmail.com>
2017-02-13 16:36:36 +0630

    adding custom field in posting items

--- resources/views/vendors/newitem.blade.php
+++ resources/views/vendors/newitem.blade.php
@@ -24,6 +24,13 @@
                     <div class="form-group">
                         <label class="control-label">Description</label>
                         <input type="text" placeholder="about product" class="form-control" /> </div>
+                    <div id="id-custom-fields">
+                        <div class="form-group">
+                            <div class="margiv-top-10">
+                                <a href="javascript:;" class="btn blue" onClick="addCustomField(); return false;"> Add More Description Fields </a>
+                            </div>
+                        </div>
+                    </div>
                     <div class="form-group">
                         <label class="control-label">Price</label>
                         <input type="text" placeholder="20000Ks" class="form-control" /> </div>
@@ -37,6 +44,36 @@
                         <a href="javascript:;" class="btn green"> Save Changes </a>
                         <a href="home" class="btn default"> Cancel </a>
                     </div>
+
+                    <script type="text/javascript">
+
+                        var customFieldIndex = 0;
+
+                        function addCustomField() {
+                            var $ = jQuery;
+                            customFieldIndex++;
+                            var html = '\
+                        <div id="id-custom-field-' + customFieldIndex + '" class="form-group" style="padding-bottom: 30px;">\
+                              <div class="col-md-5">\
+                                <input type="text" id="id-input-custom-field-key-' + customFieldIndex + '" name="custom_fields_keys[]" class="form-control" placeholder="Title" value="">\
+                              </div>\
+                              <div class="col-md-5">\
+                                                            <input type="text" id="id-input-custom-field-value-' + customFieldIndex + '" name="custom_fields_values[]" class="form-control" placeholder="Description" value="">\
+                              </div>\
+                              <div class="col-md-2">\
+                                <a href="#" class="btn btn-danger" onClick="removeCustomField(' + customFieldIndex + '); return false;"><i class="entypo-trash"></i> Remove</a>\
+                              </div>\
+                        </div>';
+                            $("#id-custom-fields").append(html);
+                        }
+                        function removeCustomField(i) {
+                            var $ = jQuery;
+                            var cf = $("#id-custom-field-" + i);
+                            if (cf != undefined) {
+                                cf.remove();
+                            }
+                        }
+                    </script>
                 </form>
             </div>
         </div>
